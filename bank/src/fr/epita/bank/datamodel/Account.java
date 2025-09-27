26c66044d2e572da74ac86e9bc409343acf28541
naveed <m.naveed@lawcus.com>
2025-09-27 17:11:12 +0500

    Complete Assignments 1, 2, and 3
    - Assignment 1: Investment account creation
    - Assignment 2: Stock buying from investment account
    - Assignment 3: BankService to centralize all actions

--- bank/src/fr/epita/bank/datamodel/Account.java
+++ bank/src/fr/epita/bank/datamodel/Account.java
@@ -1,12 +1,28 @@
 package fr.epita.bank.datamodel;
 
 public class Account {
-    String number;
-    Double balance;
+    private String number;
+    private Double balance;
 
     protected Account(String number, double balance) {
         this.number = number;
         this.balance = balance;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
     }
 
 
--- bank/src/fr/epita/bank/datamodel/InvestmentAccount.java
+++ bank/src/fr/epita/bank/datamodel/InvestmentAccount.java
@@ -1,6 +1,21 @@
 package fr.epita.bank.datamodel;
 
-public class InvestmentAccount extends Account{
+public class InvestmentAccount extends Account {
 
+    public InvestmentAccount(String accountNumber, Double balance) {
+        super(accountNumber, balance);
+    }
+
+    public void buyStock(String stockName, Double amount) {
+        Double currentBalance = getBalance();
+
+        if (currentBalance < amount) {
+            System.out.println("Insufficient balance to buy stocks!");
+            return;
+        }
+
+        System.out.println("Buying " + amount + " of " + stockName + " from account " + getNumber());
+        setBalance(currentBalance - amount);
+    }
 
 }
