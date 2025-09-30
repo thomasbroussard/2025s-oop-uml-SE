9362d00f32b1b06907db961412a8f5b813f9e2ab
Présentation <tbr@tbr.com>
2025-09-30 14:25:30 +0200

    I/O with files

--- biostat/src/fr/epita/biostat/launcher/Launcher.java
+++ biostat/src/fr/epita/biostat/launcher/Launcher.java
@@ -1,10 +1,30 @@
 package fr.epita.biostat.launcher;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //scannerPlusSystemInDemo();
+        Path path = Paths.get("biostat/biostat.csv");
+        Scanner sc = new Scanner(path);
+        List<BioStatEntry> entries = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+        System.out.println(entries.size());
+
+        sc.close();
+
+    }
+
+    private static void scannerPlusSystemInDemo() {
         System.out.println("Hello World!");
 
         Scanner sc = new Scanner(System.in);
@@ -24,6 +44,5 @@ public class Launcher {
         }
         System.out.println("your age is " + age);
         sc.close();
-
     }
 }
