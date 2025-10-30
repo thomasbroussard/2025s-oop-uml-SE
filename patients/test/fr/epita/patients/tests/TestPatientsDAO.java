6f7146d5d38707a1c1009fa149742cfc41e76b93
Présentation <tbr@tbr.com>
2025-10-30 15:30:33 +0100

    patients jdbc introduction

--- patients/test/fr/epita/patients/tests/TestPatientsDAO.java
+++ patients/test/fr/epita/patients/tests/TestPatientsDAO.java
@@ -2,6 +2,7 @@ package fr.epita.patients.tests;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TestPatientsDAO {
@@ -19,7 +20,36 @@ public class TestPatientsDAO {
                     insurance INT)
                 """;
         connection.prepareStatement(createTable).execute();
+        String insertStatement = """
+            INSERT INTO patients(hcNum,
+                    firstName, 
+                    address, 
+                    insurance) 
+            VALUES('2758965423102','test','Paris', 1)
+        """;
 
 
+        connection.prepareStatement(insertStatement).execute();
+
+        displayPatients(connection);
+
+
+        connection.close();
+
+
+    }
+
+    private static void displayPatients(Connection connection) throws SQLException {
+        String selectStatement = """
+               select hcNum,firstName,address,insurance
+               from patients
+                """;
+        ResultSet resultSet = connection.prepareStatement(selectStatement).executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("hcNum"));
+            System.out.println(resultSet.getString("firstName"));
+            System.out.println(resultSet.getString("address"));
+            System.out.println(resultSet.getString("insurance"));
+        }
     }
 }
